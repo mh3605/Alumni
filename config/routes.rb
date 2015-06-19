2b895510dc22b6f8d3ca04be9a5bb56cfe6f4382
Michelle Hao <michellehao@Jeffs-MacBook-Pro.local>
2015-06-19 09:59:55 -0400

    Built basic database tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  resources :years
+  resources :initialemployers
+  resources :departments
+  resources :faculties
+  resources :alums
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'alums#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
