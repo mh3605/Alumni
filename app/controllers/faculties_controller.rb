b7af24e1d80c77f3a310cb32117a163229dca006
Michelle Hao <michellehao@Jeffs-MacBook-Pro.local>
2015-07-13 16:30:28 -0400

    fixed department column in faculty index

--- app/controllers/faculties_controller.rb
+++ app/controllers/faculties_controller.rb
@@ -73,6 +73,6 @@ class FacultiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def faculty_params
-      params.require(:faculty).permit(:name, :email, :uid, :department_id, :about)
+      params.require(:faculty).permit(:name, :email, :uid, :department_id, :researcharea_id, :about)
     end
 end
