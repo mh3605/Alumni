3472716fdcdb94317074d2d50e76b8cc7f3e141f
Michelle Hao <michellehao@Jeffs-MacBook-Pro.local>
2015-07-29 12:49:07 -0400

    removed load and auth. from years controller

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -1,8 +1,8 @@
 class YearsController < ApplicationController
-  load_and_authorize_resource
-   skip_authorize_resource :only => [:show, :index]
-  before_filter :authenticate_user!, :except => [:index, :show]
-  before_action :set_year, only: [:show, :edit, :update, :destroy]
+  #load_and_authorize_resource
+  # skip_authorize_resource :only => [:show, :index]
+  #before_filter :authenticate_user!, :except => [:index, :show]
+  #before_action :set_year, only: [:show, :edit, :update, :destroy]
 
   # GET /years
   # GET /years.json
@@ -13,22 +13,27 @@ class YearsController < ApplicationController
   # GET /years/1
   # GET /years/1.json
   def show
+    @year = Year.find(params[:id])
   	@alums= @year.alums
   end
 
   # GET /years/new
   def new
     @year = Year.new
+    authorize! :create, Year
   end
 
   # GET /years/1/edit
   def edit
+    @year = Year.find(params[:id])
+    authorize! :update, Year
   end
 
   # POST /years
   # POST /years.json
   def create
     @year = Year.new(year_params)
+    authorize! :create, Year
 
     respond_to do |format|
       if @year.save
@@ -44,6 +49,8 @@ class YearsController < ApplicationController
   # PATCH/PUT /years/1
   # PATCH/PUT /years/1.json
   def update
+    @year = Year.find(params[:id])
+    authorize! :update, Year
     respond_to do |format|
       if @year.update(year_params)
         format.html { redirect_to @year, notice: 'Year was successfully updated.' }
@@ -58,6 +65,8 @@ class YearsController < ApplicationController
   # DELETE /years/1
   # DELETE /years/1.json
   def destroy
+    @year = Year.find(params[:id])
+    authorize! :edit, Year
     @year.destroy
     respond_to do |format|
       format.html { redirect_to years_url, notice: 'Year was successfully destroyed.' }
